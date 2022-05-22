29fb9a89a0cb9d020de6799ca0728b8916af8b9e
jttip79 <105398177+jttip79@users.noreply.github.com>
2022-05-22 19:59:49 -0400

    Add files via upload

---
+++ knowledge_check1.py
@@ -0,0 +1,13 @@
+message = "Hello World!"
+print(message)
+
+favorite_music = ['blues', 'soul', 'rock', 'funk', 'metal']
+print(favorite_music[1])
+
+bands = {}
+bands['metal'] = 'metallica'
+bands['funk'] = 'parliament'
+print(bands['metal'])
+
+pie = ('key lime', 'pecan', 'chess', 'lemon meringue')
+print(pie[0])
